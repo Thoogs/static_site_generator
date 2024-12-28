75ef8c1cbef659fb088a9051bad7b59ea1259caf
Thoogs <thoogs@pm.me>
2024-12-28 17:56:19 +0200

    Add html LeafNodes and unittests

--- src/static_site_gen/htmlnode.py
+++ src/static_site_gen/htmlnode.py
@@ -24,3 +24,19 @@ class HTMLNode:
 
     def __repr__(self):
         return f"HTMLNode({self.tag=}, {self.value=}, {self.children=}, {self.props=})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props: list = None):
+        super().__init__(tag, value, props=props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("LeafNode must have value")
+        if self.props is not None and self.tag is None:
+            raise ValueError("LeafNode with props requires a tag.")
+        if self.props is None:
+            if self.tag is None:
+                return f"{self.value}"
+            return f"<{self.tag}>{self.value}</{self.tag}>"
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
--- tests/test_htmlnode.py
+++ tests/test_htmlnode.py
@@ -1,4 +1,5 @@
-from static_site_gen.htmlnode import HTMLNode
+from static_site_gen.htmlnode import HTMLNode, LeafNode
+import pytest
 
 
 def test_htmlnode_without_props():
@@ -18,7 +19,6 @@ def test_htmlnode_multiple_props():
     value = "This is a test link"
     props = {"href": "google.com", "target": "__blank"}
     node = HTMLNode(tag=tag, value=value, props=props)
-    expected = f"HTMLNode(self.tag={tag}, self.value={value}, self.children=None, self.props={props})"
     html_props = node.props_to_html()
     assert html_props == ' href="google.com" target="__blank"'
 
@@ -30,3 +30,38 @@ def test_htmlnode_repr():
     node = HTMLNode(tag=tag, value=value, props=props)
     expected = f"HTMLNode(self.tag='{tag}', self.value='{value}', self.children=None, self.props={props})"
     assert node.__repr__() == expected
+
+
+def test_leafnode_no_tag_no_prop():
+    value = "This is a test link"
+    node = LeafNode(tag=None, value=value)
+    node_html = node.to_html()
+    expected = "This is a test link"
+    assert node_html == expected
+
+
+def test_leafnode_tag_no_prop():
+    tag = "p"
+    value = "This is a test paragraph"
+    node = LeafNode(tag=tag, value=value)
+    node_html = node.to_html()
+    expected = "<p>This is a test paragraph</p>"
+    assert node_html == expected
+
+
+def test_leafnode_tag_prop():
+    tag = "a"
+    value = "This is a test link"
+    props = {"href": "google.com", "target": "__blank"}
+    node = LeafNode(tag=tag, value=value, props=props)
+    node_html = node.to_html()
+    expected = '<a href="google.com" target="__blank">This is a test link</a>'
+    assert node_html == expected
+
+
+def test_leafnode_props_without_tag():
+    value = "This is a test link"
+    props = {"href": "google.com", "target": "__blank"}
+    node = LeafNode(tag=None, value=value, props=props)
+    with pytest.raises(ValueError, match="LeafNode with props requires a tag."):
+        node.to_html()
