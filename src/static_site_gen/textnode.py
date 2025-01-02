62606d0c1dd1dcdc3edf0b7b9a4941d4125cf07a
Thoogs <thoogs@pm.me>
2025-01-02 14:51:53 +0200

    Add text_node_to_html_node fuction

--- src/static_site_gen/textnode.py
+++ src/static_site_gen/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from static_site_gen.htmlnode import LeafNode
 
 
 class TextType(Enum):
@@ -27,3 +28,23 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+
+def text_node_to_html_node(textnode):
+    match textnode.text_type:
+        case TextType.MD_TEXT:
+            return LeafNode(tag=None, value=textnode.text)
+        case TextType.MD_BOLD:
+            return LeafNode(tag="b", value=textnode.text)
+        case TextType.MD_ITALIC:
+            return LeafNode(tag="i", value=textnode.text)
+        case TextType.MD_CODE:
+            return LeafNode(tag="code", value=textnode.text)
+        case TextType.MD_IMAGE:
+            return LeafNode(
+                tag="img", value="", props={"src": textnode.url, "alt": textnode.text}
+            )
+        case TextType.MD_LINK:
+            return LeafNode(tag="a", value=textnode.text, props={"href": textnode.url})
+        case _:
+            raise ValueError("Unsupported textnode type.")
--- tests/test_textnode.py
+++ tests/test_textnode.py
@@ -1,4 +1,6 @@
-from static_site_gen.textnode import TextNode, TextType
+import pytest
+
+from static_site_gen.textnode import TextNode, TextType, text_node_to_html_node
 
 
 def test_eq():
@@ -29,3 +31,44 @@ def test_not_eq_text():
     node = TextNode("This is a test node", TextType.MD_BOLD)
     node2 = TextNode("This is a test node but with more", TextType.MD_BOLD)
     assert node != node2
+
+
+def test_textnode_to_leafnode():
+    node = TextNode("This is a test node", TextType.MD_BOLD)
+    html_node = text_node_to_html_node(node)
+    assert html_node.value == "This is a test node"
+    assert html_node.tag == "b"
+    assert html_node.props is None
+
+
+def test_textnode_to_leafnode_with_props():
+    node = TextNode("This is a test node", TextType.MD_LINK, "google.com")
+    html_node = text_node_to_html_node(node)
+    assert html_node.value == "This is a test node"
+    assert html_node.tag == "a"
+    assert html_node.props == {"href": "google.com"}
+
+
+def test_textnode_to_leafnode_without_tag():
+    node = TextNode("This is a test node", TextType.MD_TEXT)
+    html_node = text_node_to_html_node(node)
+    assert html_node.value == "This is a test node"
+    assert html_node.tag is None
+    assert html_node.props is None
+
+
+def test_textnode_to_leafnode_image():
+    node = TextNode("This is a test image", TextType.MD_IMAGE, "https://www.boot.dev")
+    html_node = text_node_to_html_node(node)
+    assert html_node.value == ""
+    assert html_node.tag == "img"
+    assert html_node.props == {
+        "alt": "This is a test image",
+        "src": "https://www.boot.dev",
+    }
+
+
+def test_textnode_to_leafnode_unsupported_type():
+    node = TextNode("This is a text", "gibberish", "google.com")
+    with pytest.raises(ValueError, match="Unsupported textnode type."):
+        text_node_to_html_node(node)
