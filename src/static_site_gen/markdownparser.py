3968a652129b688744f135de2c703a865b8bb55b
Thoogs <thoogs@pm.me>
2025-01-05 19:40:39 +0200

    Clear up the markdownParser

--- src/static_site_gen/markdownparser.py
+++ src/static_site_gen/markdownparser.py
@@ -4,16 +4,20 @@ from static_site_gen.textnode import TextType, TextNode
 def split_nodes_delimiter(old_nodes: list, delimiter: str, text_type: TextType) -> list:
     new_nodes = []
     for node in old_nodes:
+        # We only want to edit text nodes, so others get immediately added as is
         if node.text_type != TextType.MD_TEXT:
             new_nodes.append(node)
             continue
-        split_node = node.text.split(delimiter)
-        if len(split_node) % 2 == 0:
+        sections = node.text.split(delimiter)
+        # Delimiter needs to have matching pair to be valid markdown
+        if len(sections) % 2 == 0:
             raise SyntaxError("Invalid markdown syntax")
-        for idx in range(len(split_node)):
+        # each section needs to be converted to node
+        for idx in range(len(sections)):
             if idx % 2 == 1:
-                new_nodes.append(TextNode(split_node[idx], text_type))
+                new_nodes.append(TextNode(sections[idx], text_type))
             else:
-                if len(split_node[idx]) != 0:
-                    new_nodes.append(TextNode(split_node[idx], TextType.MD_TEXT))
+                # Make sure the section is more than empty string
+                if len(sections[idx]) != 0:
+                    new_nodes.append(TextNode(sections[idx], TextType.MD_TEXT))
     return new_nodes
--- tests/test_markdownparser.py
+++ tests/test_markdownparser.py
@@ -4,13 +4,17 @@ from static_site_gen.textnode import TextNode, TextType
 
 
 def test_split_nodes_delimiter_bold():
-    old_node = [TextNode("This is text with a **bolded phrase** in the middle", TextType.MD_TEXT)]
+    old_node = [
+        TextNode(
+            "This is text with a **bolded phrase** in the middle", TextType.MD_TEXT
+        )
+    ]
     delimiter = "**"
     expected = [
         TextNode("This is text with a ", TextType.MD_TEXT),
         TextNode("bolded phrase", TextType.MD_BOLD),
         TextNode(" in the middle", TextType.MD_TEXT),
-        ]
+    ]
 
     nodes = split_nodes_delimiter(old_node, delimiter, TextType.MD_BOLD)
     assert nodes == expected
@@ -22,14 +26,18 @@ def test_split_nodes_delimiter_code():
     expected = [
         TextNode("This is text with a ", TextType.MD_TEXT),
         TextNode("code block", TextType.MD_CODE),
-        ]
+    ]
 
     nodes = split_nodes_delimiter(old_node, delimiter, TextType.MD_CODE)
     assert nodes == expected
 
 
 def test_split_nodes_delimiter_bold_and_italic():
-    old_node = [TextNode("This is text with a **bolded phrase** in *the* middle", TextType.MD_TEXT)]
+    old_node = [
+        TextNode(
+            "This is text with a **bolded phrase** in *the* middle", TextType.MD_TEXT
+        )
+    ]
     delimiter = "**"
     expected = [
         TextNode("This is text with a ", TextType.MD_TEXT),
@@ -37,7 +45,7 @@ def test_split_nodes_delimiter_bold_and_italic():
         TextNode(" in ", TextType.MD_TEXT),
         TextNode("the", TextType.MD_ITALIC),
         TextNode(" middle", TextType.MD_TEXT),
-        ]
+    ]
 
     nodes = split_nodes_delimiter(old_node, delimiter, TextType.MD_BOLD)
     delimiter = "*"
@@ -46,14 +54,18 @@ def test_split_nodes_delimiter_bold_and_italic():
 
 
 def test_split_nodes_delimiter_multiple_blocks():
-    old_node = [TextNode("This is text with a `code block`, more text with `code`", TextType.MD_TEXT)]
+    old_node = [
+        TextNode(
+            "This is text with a `code block`, more text with `code`", TextType.MD_TEXT
+        )
+    ]
     delimiter = "`"
     expected = [
         TextNode("This is text with a ", TextType.MD_TEXT),
         TextNode("code block", TextType.MD_CODE),
         TextNode(", more text with ", TextType.MD_TEXT),
         TextNode("code", TextType.MD_CODE),
-        ]
+    ]
 
     nodes = split_nodes_delimiter(old_node, delimiter, TextType.MD_CODE)
     assert nodes == expected
@@ -61,9 +73,11 @@ def test_split_nodes_delimiter_multiple_blocks():
 
 def test_split_nodes_delimiter_multiple_nodes():
     old_node = [
-            TextNode("This is text with a `code block`, more text with `code`", TextType.MD_TEXT),
-            TextNode("Another node with more `code`", TextType.MD_TEXT),
-            ]
+        TextNode(
+            "This is text with a `code block`, more text with `code`", TextType.MD_TEXT
+        ),
+        TextNode("Another node with more `code`", TextType.MD_TEXT),
+    ]
     delimiter = "`"
     expected = [
         TextNode("This is text with a ", TextType.MD_TEXT),
@@ -72,16 +86,24 @@ def test_split_nodes_delimiter_multiple_nodes():
         TextNode("code", TextType.MD_CODE),
         TextNode("Another node with more ", TextType.MD_TEXT),
         TextNode("code", TextType.MD_CODE),
-        ]
+    ]
 
     nodes = split_nodes_delimiter(old_node, delimiter, TextType.MD_CODE)
     assert nodes == expected
 
 
 def test_split_nodes_delimiter_non_text_node():
-    old_node = [TextNode("This is text with a `code block`, more text with `code`", TextType.MD_CODE)]
+    old_node = [
+        TextNode(
+            "This is text with a `code block`, more text with `code`", TextType.MD_CODE
+        )
+    ]
     delimiter = "`"
-    expected = [TextNode("This is text with a `code block`, more text with `code`", TextType.MD_CODE)]
+    expected = [
+        TextNode(
+            "This is text with a `code block`, more text with `code`", TextType.MD_CODE
+        )
+    ]
 
     nodes = split_nodes_delimiter(old_node, delimiter, TextType.MD_CODE)
     assert nodes == expected
